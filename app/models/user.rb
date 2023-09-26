5bfeb7c577ec5827c3915d9781781d4bcc95035f
Abdelaziz Mohamed Ali <abdelaziz.dv@gmail.com>
2023-09-26 09:40:46 +0300

    Setup associations for user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :posts, dependent: :destroy, foreign_key: 'author_id'
+  has_many :likes, dependent: :destroy, foreign_key: 'user_id'
+  has_many :comments, dependent: :destroy, foreign_key: 'user_id'
 end
