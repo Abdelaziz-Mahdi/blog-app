0ab52f23432fa0b53602aeff12aee75650263781
Abdelaziz Mohamed Ali <abdelaziz.dv@gmail.com>
2023-09-26 09:40:38 +0300

    Setup associations for post class

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+  belongs_to :user
+  has_many :comments, foreign_key: 'post_id', dependent: :destroy
+  has_many :likes, foreign_key: 'post_id', dependent: :destroy
 end
