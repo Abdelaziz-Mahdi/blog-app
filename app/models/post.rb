c708fafd9e18c0e5425be4c2f81a48f94d9d7c81
Abdelaziz Mohamed Ali <abdelaziz.dv@gmail.com>
2023-09-28 12:20:40 +0300

    Create custom method to update posts counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :comments, foreign_key: 'post_id', dependent: :destroy
   has_many :likes, foreign_key: 'post_id', dependent: :destroy
+
+  def update_posts_counter
+    author.update(posts_counter: author.posts.count)
+  end
 end
