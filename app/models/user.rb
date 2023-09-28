ea3667a6e850989233f8982a346e7bcef7d44bb0
Abdelaziz Mohamed Ali <abdelaziz.dv@gmail.com>
2023-09-28 12:22:43 +0300

    Create custom method to return 3 most recent posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy, foreign_key: 'author_id'
   has_many :likes, dependent: :destroy, foreign_key: 'user_id'
   has_many :comments, dependent: :destroy, foreign_key: 'user_id'
+
+  def three_most_recent_posts
+    posts.order(created_at: :desc).limit(3)
+  end
 end
