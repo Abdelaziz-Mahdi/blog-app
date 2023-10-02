d91c9182912e978831b525c30a611b4a75e9f06d
Abdelaziz Mohamed Ali <abdelaziz.dv@gmail.com>
2023-10-02 13:41:43 +0300

    Fix linters

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,7 +7,6 @@ class Post < ApplicationRecord
   validates :comments_counter, numericality: { greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { greater_than_or_equal_to: 0 }
 
-
   def update_posts_counter
     author.update(posts_counter: author.posts.count)
   end
