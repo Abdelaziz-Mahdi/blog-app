13356c6b4c434ad5901e7c94f4f87c36423257e4
Abdelaziz Mohamed Ali <abdelaziz.dv@gmail.com>
2023-10-03 19:18:10 +0300

    Create routes to users and posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users do
+    resources :posts
+  end
+  root "users#index"
 end
