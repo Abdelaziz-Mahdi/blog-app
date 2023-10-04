d7effccc762c32b5067dc4d06d4234173abff8ad
Abdelaziz Mohamed Ali <abdelaziz.dv@gmail.com>
2023-10-04 16:55:16 +0300

    Update routes to handle only the 4 required URLs

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :users do
-    resources :posts
+  resources :users, only: [:index, :show]   do
+    resources :posts, only: [:index, :show] 
   end
   root "users#index"
 end
