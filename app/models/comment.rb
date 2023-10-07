4dd9f2f4e8034170b023fdd44c91457dac852262
Abdelaziz Mohamed Ali <abdelaziz.dv@gmail.com>
2023-10-07 17:58:12 +0300

    Update comment counter using method

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
+  after_create :update_comments_counter
+  after_destroy :update_comments_counter
+
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
   belongs_to :post
 
