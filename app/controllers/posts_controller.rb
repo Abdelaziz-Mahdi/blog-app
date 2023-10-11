6e7eac23ad8719ab1ecdeccfdfe27e1665f48456
Abdelaziz Mohamed Ali <abdelaziz.dv@gmail.com>
2023-10-11 11:59:04 +0300

    Create controller methods for post new and create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,22 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params.require(:post).permit(:title, :text))
+    @post.author_id = current_user.id
+    if @post.save
+      flash[:success] = 'Post created successfully!'
+      redirect_to user_posts_path
+    else
+      redirect_to request.referrer
+      flash[:warning] = 'Post not created!'
+    end
+  end
+
   def show
     @post = Post.find_by_id(params[:id])
   end
