fe00034adf77a98dc9d398b9f56fb19a7243a878
Abdelaziz Mohamed Ali <abdelaziz.dv@gmail.com>
2023-10-11 11:57:56 +0300

    Add routes for new and create

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   resources :users, only: [:index, :show]   do
-    resources :posts, only: [:index, :show] 
+    resources :posts, only: [:index, :new, :show]
+    resources :posts, only: [:create], path: "posts/new"
   end
   root "users#index"
 end
