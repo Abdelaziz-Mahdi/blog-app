28757964beacd85af577ff708ff9c33cdd7f92af
Abdelaziz Mohamed Ali <abdelaziz.dv@gmail.com>
2023-10-12 08:26:17 +0300

    Add routes for comment

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   resources :users, only: [:index, :show]   do
-    resources :posts, only: [:index, :new, :show]
+    resources :posts, only: [:index, :new, :show] do
+      resources :comments, only: [:new]
+      resources :comments, only: [:create], path: "comments/new"
+    end
     resources :posts, only: [:create], path: "posts/new"
   end
   root "users#index"
