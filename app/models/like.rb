8391d1536bb6148e63d8aab6fbfc071865c17c1a
Abdelaziz Mohamed Ali <abdelaziz.dv@gmail.com>
2023-10-13 15:17:55 +0300

    Add validation prevent multiple likes by same user

--- app/models/like.rb
+++ app/models/like.rb
@@ -5,6 +5,9 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
   belongs_to :post
 
+  # prevent multiple likes by same user on same post
+  validates :author, uniqueness: { scope: :post, message: "You can't like a post more than once" }
+
   def update_likes_counter
     post.update(likes_counter: post.likes.count)
   end
