f0488c3c6540e5cc68d24aaeca3653000b318c75
Abdelaziz Mohamed Ali <abdelaziz.dv@gmail.com>
2023-10-16 09:17:03 +0300

    Fix post create route

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   resources :users, only: [:index, :show]   do
-    resources :posts, only: [:index, :new, :show] do
+    resources :posts, only: [:index, :new, :create, :show] do
       resources :likes, only: [:create], path: ""
       resources :likes, only: [:destroy], path: ""
       resources :comments, only: [:create], path: "comment"
     end
-    resources :posts, only: [:create], path: "posts/new"
   end
   root "users#index"
 end
