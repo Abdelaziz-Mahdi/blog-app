6596e45db26b0a9eca132e573332ade31bf7ddb0
Abdelaziz Mohamed Ali <abdelaziz.dv@gmail.com>
2023-11-08 01:59:09 +0200

    Migrate user changes after install devise

--- db/migrate/20230923022532_create_users.rb
+++ db/migrate/20230923022532_create_users.rb
@@ -2,8 +2,8 @@ class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :name
-      t.string :photo
-      t.text :bio
+      t.string :photo, default: "https://ui-avatars.com/api/?name=Df"
+      t.text :bio, default: ""
       t.integer :posts_counter, default: 0, null: false
 
       t.timestamps
